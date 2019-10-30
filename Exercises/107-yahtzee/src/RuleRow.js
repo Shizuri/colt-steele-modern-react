93726f127c02c465374444fdd1523503ecb9396e
Zdravko Mavkov <mavkov@hotmail.com>
2019-10-30 09:45:47 +0100

    107. EXERCISE Yahtzee, Part Seven: Add Descriptions

--- Exercises/107-yahtzee/src/RuleRow.js
+++ Exercises/107-yahtzee/src/RuleRow.js
@@ -8,9 +8,55 @@ class RuleRow extends Component {
       <tr className={this.props.score === undefined ? "RuleRow RuleRow-active" : "RuleRow RuleRow-disabled"}
         onClick={this.props.score === undefined ? this.props.doScore : null}>
         <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+        <td className="RuleRow-score">{this.props.score === undefined ? this.description(this.props.name) : this.props.score}</td>
       </tr>
     )
+  }
+
+  description(name) {
+    switch (name) {
+      case 'Ones':
+        return 'Score 1 for every 1';
+        break;
+      case 'Twos':
+        return 'Score 2 for every 2';
+        break;
+      case 'Threes':
+        return 'Score 3 for every 3';
+        break;
+      case 'Fours':
+        return 'Score 4 for every 4';
+        break;
+      case 'Fives':
+        return 'Score 5 for every 6';
+        break;
+      case 'Sixes':
+        return 'Score 6 for every 6';
+        break;
+      case 'Three of Kind':
+        return 'If 3+ of one value, score sum of all dice';
+        break;
+      case 'Four of Kind':
+        return 'If 4+ of one value, score sum of all dice';
+        break;
+      case 'Full House':
+        return 'If 3 of one value and 2 of another, score 25';
+        break;
+      case 'Small Straight':
+        return 'If 4+ values in a row, score 30';
+        break;
+      case 'Large Straight':
+        return 'If 5 values in a row, score 40';
+        break;
+      case 'Yahtzee':
+        return 'SIf all values match, score 50';
+        break;
+      case 'Chance':
+        return 'Score sum of all dice';
+        break;
+      default:
+        return 'description';
+    }
   }
 }
 
