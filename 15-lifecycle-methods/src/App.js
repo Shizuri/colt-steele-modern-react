4aa4cd11bce3d44a107b35a16b9754a989e2a778
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-01 13:09:36 +0100

    118. Loading Data Via AJAX

--- 15-lifecycle-methods/src/App.js
+++ 15-lifecycle-methods/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Timer from './Timer';
 import './App.css';
+import ZenQuote from './ZenQuote';
 
 function App() {
   return (
     <div className="App">
       <Timer />
+      <ZenQuote />
     </div>
   );
 }
