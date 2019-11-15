c47108e5cf1377b2220c04bb432c6d51537359a1
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-15 08:28:21 +0100

    146. Intro to the Link Component

--- 16-routed/src/App.js
+++ 16-routed/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Dog from './Dog';
 import Contact from './Contact';
 import About from './About';
@@ -11,6 +11,15 @@ class App extends Component {
     render() {
         return (
             <div className="App">
+                <nav className='App-nav'>
+                    <Link className='App-link' to='/'>About</Link>
+                    <Link className='App-link' to='/dog'>Dog</Link>
+                    <Link className='App-link' to='/contact'>Contact</Link>
+                    <hr />
+                    <a href="/dog">Dog</a>
+                    <a href="/contact">Contact</a>
+                    <a href="/">About</a>
+                </nav>
                 <Switch>
                     <Route exact path='/' component={About} />
                     <Route exact path='/dog' component={Dog} />
