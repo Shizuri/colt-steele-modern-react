8b8c7e305136fa438a89464f0baa52e423414ee1
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-19 10:07:40 +0100

    160. Comparing History and Redirect

--- 17-router-patterns/src/Food.js
+++ 17-router-patterns/src/Food.js
@@ -5,10 +5,13 @@ import './Food.css';
 class Food extends Component {
     render() {
         const name = this.props.match.params.name;
+        if (/\d/.test(name)) {
+            this.props.history.push('/notfundYULHLJK');
+        }
         const url = `https://source.unsplash.com/1600x900/?${name}`;
         return (
             <div className='Food'>
-                {/\d/.test(name) ? <Redirect to='/' /> :
+                {/\d/.test(name) ? <Redirect to='/notfound' /> :
                     <div>
                         <h1>I love to eat {name}</h1>
                         <img src={url} alt={name} />
