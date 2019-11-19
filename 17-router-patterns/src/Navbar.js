86f1864ab1920faf287aa32a8249a8bd4fd4ff0c
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-19 10:26:00 +0100

    162. Implementing a Back Button

--- 17-router-patterns/src/Navbar.js
+++ 17-router-patterns/src/Navbar.js
@@ -5,6 +5,7 @@ class Navbar extends Component {
     constructor(props){
         super(props);
         this.handleLogin = this.handleLogin.bind(this);
+        this.handleBack = this.handleBack.bind(this);
     }
 
     handleLogin() {
@@ -12,10 +13,15 @@ class Navbar extends Component {
         this.props.history.push('/food/salmon');
     }
 
+    handleBack() {
+        this.props.history.goBack();
+    }
+
     render() {
         return (
             <div>
                 <button onClick={this.handleLogin}>Log In</button>
+                <button onClick={this.handleBack}>go back</button>
             </div>
         );
     }
