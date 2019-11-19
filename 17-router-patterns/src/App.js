af452957e329e0180aefae8038e0b1396a4d80b1
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-19 08:10:29 +0100

    155. Multiple Route Params

--- 17-router-patterns/src/App.js
+++ 17-router-patterns/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Food from './Food';
+import Meal from './Meal';
 import { Route } from 'react-router-dom';
 
 function App() {
@@ -9,9 +10,12 @@ function App() {
             <Route exact path='/food/:name' render={(routeProps) => <Food authenticated={true} {...routeProps}/>} />
 
             {/* <Route exact path='/food/:name' component={Food} /> */}
+            {/* ↑ This will not work if you need to pass additional props */}
 
             {/* <Route exact path='/food/:name' render={(routeProps) => <Food {...routeProps}
             stuff={routeProps}/>} /> */}
+
+            <Route exact path='/food/:foodName/drink/:drinkName' component={Meal}/>
         </div>
     );
 }
