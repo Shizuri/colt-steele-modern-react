ae412ac507e7c7b8cc60f7d7f1d4083aff22be79
Zdravko Mavkov <mavkov@hotmail.com>
2019-11-19 08:19:30 +0100

    156. Adding a 404 Not Found Route

--- 17-router-patterns/src/App.js
+++ 17-router-patterns/src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './App.css';
 import Food from './Food';
 import Meal from './Meal';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
-            <Route exact path='/food/:name' render={(routeProps) => <Food authenticated={true} {...routeProps}/>} />
+            <Switch>
+                <Route exact path='/food/:name' render={(routeProps) => <Food authenticated={true} {...routeProps} />} />
 
-            {/* <Route exact path='/food/:name' component={Food} /> */}
-            {/* ↑ This will not work if you need to pass additional props */}
+                {/* <Route exact path='/food/:name' component={Food} /> */}
+                {/* ↑ This will not work if you need to pass additional props */}
 
-            {/* <Route exact path='/food/:name' render={(routeProps) => <Food {...routeProps}
-            stuff={routeProps}/>} /> */}
+                {/* <Route exact path='/food/:name' render={(routeProps) => <Food {...routeProps} stuff={routeProps}/>} /> */}
 
-            <Route exact path='/food/:foodName/drink/:drinkName' component={Meal}/>
+                <Route exact path='/food/:foodName/drink/:drinkName' component={Meal} />
+                <Route exact path='/' render={() => <h1>Home page.</h1>}/>
+                <Route render={() => <h1>ERROR! PAGE NOT FOUND!</h1>} />
+            </Switch>
         </div>
     );
 }
