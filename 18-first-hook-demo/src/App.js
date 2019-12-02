bc554c355783102037367c6799fddaf23dd83e10
Zdravko Mavkov <mavkov@hotmail.com>
2019-12-02 14:05:10 +0100

    249. Building a Custom Hook: useInputState

--- 18-first-hook-demo/src/App.js
+++ 18-first-hook-demo/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import CounterClass from './CounterClass';
 import CounterHooks from './CounterHooks';
 import Toggler from './Toggler';
+import SimpleFormClass from './SimpleFormClass';
+import SimpleFormHooks from './SimpleFormHooks';
+import SimpleFormInputHook from './SimpleFormInputHook';
 import './App.css';
 
 function App() {
@@ -12,6 +15,12 @@ function App() {
       <CounterHooks />
       <hr />
       <Toggler />
+      <hr />
+      <SimpleFormClass />
+      <hr />
+      <SimpleFormHooks />
+      <hr />
+      <SimpleFormInputHook />
     </div>
   );
 }
