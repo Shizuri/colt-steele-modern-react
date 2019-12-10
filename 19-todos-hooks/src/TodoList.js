67b46be99943630b3c58272c6311c9121bb8eec0
Zdravko Mavkov <mavkov@hotmail.com>
2019-12-10 10:45:29 +0100

    255. Toggling and Deletion w/ Hooks. GOING BACK TO CODE ALONG.

--- 19-todos-hooks/src/TodoList.js
+++ 19-todos-hooks/src/TodoList.js
@@ -3,17 +3,21 @@ import Todo from './Todo';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 
-import Divider from '@material-ui/core/Divider';
+// import Divider from '@material-ui/core/Divider';
 
-function TodoList(props) {
+function TodoList({ todos, removeTodo, toggleTodo }) {
     return (
         <Paper>
             <List>
-                {props.todos.map(todo => (
-                    <>
-                    <Todo task={todo.task} key={todo.id} completed={todo.completed} />
-                    <Divider />
-                    </>
+                {todos.map(todo => (
+                    <Todo
+                        id={todo.id}
+                        task={todo.task}
+                        key={todo.id}
+                        completed={todo.completed}
+                        removeTodo={removeTodo}
+                        toggleTodo={toggleTodo}
+                    />
                 ))}
             </List>
         </Paper>
